2934e61a44b5582ffc8661de1bd5eb6d4263e5ea
Josh Marinacci <joshua@marinacci.org>
2025-04-15 12:29:00 -0700

    formatting fixes
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

--- hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/CryptoTransferHandlerTest.java
+++ hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/CryptoTransferHandlerTest.java
@@ -669,8 +669,8 @@ class CryptoTransferHandlerTest extends CryptoTransferHandlerTestBase {
         CryptoCreateStreamBuilder cryptoCreateRecordBuilder = mock(CryptoCreateStreamBuilder.class);
 
         given(handleContext.dispatch(
-                argThat(options -> CryptoCreateStreamBuilder.class.equals(options.streamBuilderType())
-                        && spenderId.equals(options.payerId()))))
+                        argThat(options -> CryptoCreateStreamBuilder.class.equals(options.streamBuilderType())
+                                && spenderId.equals(options.payerId()))))
                 .will((invocation) -> {
                     final var copy =
                             account.copyBuilder().accountId(hbarReceiverId).build();
@@ -711,7 +711,6 @@ class CryptoTransferHandlerTest extends CryptoTransferHandlerTestBase {
         when(feeCalculator.addBytesPerTransaction(anyLong())).thenReturn(feeCalculator);
         when(feeCalculator.addRamByteSeconds(anyLong())).thenReturn(feeCalculator);
         when(feeCalculator.calculate()).thenReturn(fees);
-
 
         // this confirms that the exception thrown by customFeeAssessor.assessNumberOfCustomFees() is
         // handled properly by calculateFees()
