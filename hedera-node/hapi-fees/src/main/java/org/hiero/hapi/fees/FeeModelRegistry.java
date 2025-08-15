74cce4ac0d2295ed3ac15cd4b4195f22aba671c7
Josh Marinacci <joshua@marinacci.org>
2025-08-15 13:15:49 -0700

    add more file tests
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

--- hedera-node/hapi-fees/src/main/java/org/hiero/hapi/fees/FeeModelRegistry.java
+++ hedera-node/hapi-fees/src/main/java/org/hiero/hapi/fees/FeeModelRegistry.java
@@ -13,6 +13,8 @@ import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_UPDATE_TOP
 import static com.hedera.hapi.node.base.HederaFunctionality.FILE_APPEND;
 import static com.hedera.hapi.node.base.HederaFunctionality.FILE_CREATE;
 import static com.hedera.hapi.node.base.HederaFunctionality.FILE_DELETE;
+import static com.hedera.hapi.node.base.HederaFunctionality.FILE_GET_CONTENTS;
+import static com.hedera.hapi.node.base.HederaFunctionality.FILE_GET_INFO;
 import static com.hedera.hapi.node.base.HederaFunctionality.FILE_UPDATE;
 
 public class FeeModelRegistry {
@@ -26,6 +28,8 @@ public class FeeModelRegistry {
         registry.put(FILE_UPDATE, new BaseFeeModel(FILE_UPDATE, "Update file"));
         registry.put(FILE_APPEND, new BaseFeeModel(FILE_APPEND, "Append to file"));
         registry.put(FILE_DELETE, new BaseFeeModel(FILE_DELETE, "Delete file"));
+        registry.put(FILE_GET_CONTENTS, new BaseFeeModel(FILE_GET_CONTENTS, "Get file contents"));
+        registry.put(FILE_GET_INFO, new BaseFeeModel(FILE_GET_INFO, "Get file info"));
     }
 
     public static FeeModel lookupModel(HederaFunctionality service) {
