73ca6b11e4c0dbbd0e4cd09432c00b62703bf659
Josh Marinacci <joshua@marinacci.org>
2025-08-15 12:57:07 -0700

    add delete topic

--- hedera-node/hapi-fees/src/main/java/org/hiero/hapi/fees/FeeModelRegistry.java
+++ hedera-node/hapi-fees/src/main/java/org/hiero/hapi/fees/FeeModelRegistry.java
@@ -7,20 +7,22 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_CREATE_TOPIC;
+import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_DELETE_TOPIC;
 import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_SUBMIT_MESSAGE;
 import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_UPDATE_TOPIC;
 
 public class FeeModelRegistry {
-    public static final Map<HederaFunctionality, FeeModel> registry = new LinkedHashMap<>();
+    private static final Map<HederaFunctionality, FeeModel> registry = new LinkedHashMap<>();
     static {
         registry.put(CONSENSUS_CREATE_TOPIC, new BaseFeeModel(CONSENSUS_CREATE_TOPIC, "Create a new topic"));
         registry.put(CONSENSUS_UPDATE_TOPIC, new BaseFeeModel(CONSENSUS_UPDATE_TOPIC,"Update topic"));
         registry.put(CONSENSUS_SUBMIT_MESSAGE, new BaseFeeModel(CONSENSUS_SUBMIT_MESSAGE, "Submit message"));
+        registry.put(CONSENSUS_DELETE_TOPIC, new BaseFeeModel(CONSENSUS_DELETE_TOPIC, "Delete topic"));
     }
 
     public static FeeModel lookupModel(HederaFunctionality service) {
         if(!registry.containsKey(service)) {
-            throw new Error("no registered model found for service " + service);
+            throw new Error("No registered model found for service " + service);
         }
         return registry.get(service);
     }
