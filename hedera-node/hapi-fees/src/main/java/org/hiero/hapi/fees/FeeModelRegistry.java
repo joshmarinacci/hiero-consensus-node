5cd181abfb2d80656eb5bbd2fd48096a62c1a1d3
Josh Marinacci <joshua@marinacci.org>
2025-08-15 13:12:38 -0700

    add file tests
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

--- hedera-node/hapi-fees/src/main/java/org/hiero/hapi/fees/FeeModelRegistry.java
+++ hedera-node/hapi-fees/src/main/java/org/hiero/hapi/fees/FeeModelRegistry.java
@@ -10,14 +10,22 @@ import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_CREATE_TOP
 import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_DELETE_TOPIC;
 import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_SUBMIT_MESSAGE;
 import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_UPDATE_TOPIC;
+import static com.hedera.hapi.node.base.HederaFunctionality.FILE_APPEND;
+import static com.hedera.hapi.node.base.HederaFunctionality.FILE_CREATE;
+import static com.hedera.hapi.node.base.HederaFunctionality.FILE_DELETE;
+import static com.hedera.hapi.node.base.HederaFunctionality.FILE_UPDATE;
 
 public class FeeModelRegistry {
     private static final Map<HederaFunctionality, FeeModel> registry = new LinkedHashMap<>();
     static {
         registry.put(CONSENSUS_CREATE_TOPIC, new BaseFeeModel(CONSENSUS_CREATE_TOPIC, "Create a new topic"));
         registry.put(CONSENSUS_UPDATE_TOPIC, new BaseFeeModel(CONSENSUS_UPDATE_TOPIC,"Update topic"));
-        registry.put(CONSENSUS_SUBMIT_MESSAGE, new BaseFeeModel(CONSENSUS_SUBMIT_MESSAGE, "Submit message"));
+        registry.put(CONSENSUS_SUBMIT_MESSAGE, new BaseFeeModel(CONSENSUS_SUBMIT_MESSAGE, "Submit message to topic"));
         registry.put(CONSENSUS_DELETE_TOPIC, new BaseFeeModel(CONSENSUS_DELETE_TOPIC, "Delete topic"));
+        registry.put(FILE_CREATE, new BaseFeeModel(FILE_CREATE, "Create file"));
+        registry.put(FILE_UPDATE, new BaseFeeModel(FILE_UPDATE, "Update file"));
+        registry.put(FILE_APPEND, new BaseFeeModel(FILE_APPEND, "Append to file"));
+        registry.put(FILE_DELETE, new BaseFeeModel(FILE_DELETE, "Delete file"));
     }
 
     public static FeeModel lookupModel(HederaFunctionality service) {
---
+++ hedera-node/hapi-fees/src/test/java/org/hiero/hapi/fees/apis/file/FileTests.java
@@ -0,0 +1,145 @@
+package org.hiero.hapi.fees.apis.file;
+
+import org.hiero.hapi.fees.FeeModel;
+import org.hiero.hapi.fees.FeeModelRegistry;
+import org.hiero.hapi.fees.FeeResult;
+import org.hiero.hapi.fees.MockExchangeRate;
+import org.hiero.hapi.support.fees.Extra;
+import org.hiero.hapi.support.fees.FeeSchedule;
+import org.hiero.hapi.support.fees.NetworkFee;
+import org.hiero.hapi.support.fees.NodeFee;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_CREATE_TOPIC;
+import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_DELETE_TOPIC;
+import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_SUBMIT_MESSAGE;
+import static com.hedera.hapi.node.base.HederaFunctionality.CONSENSUS_UPDATE_TOPIC;
+import static com.hedera.hapi.node.base.HederaFunctionality.FILE_APPEND;
+import static com.hedera.hapi.node.base.HederaFunctionality.FILE_CREATE;
+import static com.hedera.hapi.node.base.HederaFunctionality.FILE_DELETE;
+import static com.hedera.hapi.node.base.HederaFunctionality.FILE_UPDATE;
+import static org.hiero.hapi.fees.FeeScheduleUtils.makeExtraDef;
+import static org.hiero.hapi.fees.FeeScheduleUtils.makeExtraIncluded;
+import static org.hiero.hapi.fees.FeeScheduleUtils.makeService;
+import static org.hiero.hapi.fees.FeeScheduleUtils.makeServiceFee;
+import static org.hiero.hapi.fees.FeeScheduleUtils.validate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FileTests {
+    static FeeSchedule feeSchedule;
+    @BeforeAll
+    static void setup() {
+        feeSchedule = FeeSchedule.DEFAULT.copyBuilder()
+                .definedExtras(
+                        makeExtraDef(Extra.SIGNATURES,1),
+                        makeExtraDef(Extra.BYTES,1),
+                        makeExtraDef(Extra.KEYS,1)
+                )
+                .nodeFee(NodeFee.DEFAULT.copyBuilder().baseFee(1).extras(
+                        makeExtraIncluded(Extra.BYTES,10),
+                        makeExtraIncluded(Extra.SIGNATURES,1)
+                ).build())
+                .networkFeeRatio(NetworkFee.DEFAULT.copyBuilder().multiplier(2).build())
+                .serviceFees(
+                        makeService("File",
+                                makeServiceFee(FILE_CREATE,11,
+                                        makeExtraIncluded(Extra.SIGNATURES, 1),
+                                        makeExtraIncluded(Extra.KEYS, 1),
+                                        makeExtraIncluded(Extra.BYTES, 1024)
+                                ),
+                                makeServiceFee(FILE_UPDATE,11,
+                                        makeExtraIncluded(Extra.SIGNATURES, 1),
+                                        makeExtraIncluded(Extra.KEYS, 1),
+                                        makeExtraIncluded(Extra.BYTES, 1024)
+                                ),
+                                makeServiceFee(FILE_APPEND,11,
+                                        makeExtraIncluded(Extra.SIGNATURES, 1),
+                                        makeExtraIncluded(Extra.KEYS, 1),
+                                        makeExtraIncluded(Extra.BYTES, 1024)
+                                ),
+                                makeServiceFee(FILE_DELETE,11,
+                                        makeExtraIncluded(Extra.SIGNATURES, 1),
+                                        makeExtraIncluded(Extra.KEYS, 1),
+                                        makeExtraIncluded(Extra.BYTES, 1024)
+                                )
+                        )
+                )
+                .build();
+    }
+
+    @Test
+    void fileCreateFee() {
+        FeeModel model = FeeModelRegistry.lookupModel(FILE_CREATE);
+        Map<String, Object> params = new HashMap<>();
+        params.put(Extra.SIGNATURES.name(), 1L);
+        params.put(Extra.BYTES.name(), 500L);
+        params.put(Extra.KEYS.name(), 1L);
+        assertTrue(validate(feeSchedule),"Fee schedule failed validation");
+        FeeResult fee = model.computeFee(params, new MockExchangeRate().activeRate(), feeSchedule);
+        System.out.println(fee);
+        assertEquals(11 + 0 + (1+500-10)*3,fee.total());
+    }
+    @Test
+    void fileCreateBigFileFee() {
+        FeeModel model = FeeModelRegistry.lookupModel(FILE_CREATE);
+        Map<String, Object> params = new HashMap<>();
+        params.put(Extra.SIGNATURES.name(), 1L);
+        params.put(Extra.BYTES.name(), 2000L);
+        params.put(Extra.KEYS.name(), 1L);
+        FeeResult fee = model.computeFee(params, new MockExchangeRate().activeRate(), feeSchedule);
+        System.out.println(fee);
+        assertEquals(11 + (2000-1024)*1 + (1+2000-10)*3,fee.total());
+    }
+
+    @Test
+    void fileUpdateFee() {
+        FeeModel model = FeeModelRegistry.lookupModel(FILE_UPDATE);
+        Map<String, Object> params = new HashMap<>();
+        params.put(Extra.SIGNATURES.name(), 1L);
+        params.put(Extra.BYTES.name(), 500L);
+        params.put(Extra.KEYS.name(), 1L);
+        assertTrue(validate(feeSchedule),"Fee schedule failed validation");
+        FeeResult fee = model.computeFee(params, new MockExchangeRate().activeRate(), feeSchedule);
+        System.out.println(fee);
+        assertEquals(11 + 0 + (1+500-10)*3,fee.total());
+    }
+
+    @Test
+    void fileDeleteFee() {
+        FeeModel model = FeeModelRegistry.lookupModel(FILE_DELETE);
+        Map<String, Object> params = new HashMap<>();
+        params.put(Extra.SIGNATURES.name(), 1L);
+        params.put(Extra.BYTES.name(), 100L);
+        params.put(Extra.KEYS.name(), 1L);
+        FeeResult fee = model.computeFee(params, new MockExchangeRate().activeRate(), feeSchedule);
+        System.out.println(fee);
+        assertEquals(11 + 0 + (1+100-10)*3,fee.total());
+    }
+
+    @Test
+    void fileAppendFee() {
+        FeeModel model = FeeModelRegistry.lookupModel(FILE_APPEND);
+        Map<String, Object> params = new HashMap<>();
+        params.put(Extra.SIGNATURES.name(), 1L);
+        params.put(Extra.BYTES.name(), 3000L);
+        params.put(Extra.KEYS.name(), 1L);
+        assertTrue(validate(feeSchedule),"Fee schedule failed validation");
+        FeeResult fee = model.computeFee(params, new MockExchangeRate().activeRate(), feeSchedule);
+        System.out.println(fee);
+        assertEquals(11 + (3000-1024)*1 + (1+3000-10)*3,fee.total());
+    }
+
+    @Test
+    void fileGetContentsFee() {
+
+    }
+    @Test
+    void fileGetInfoFee() {
+
+    }
+}
