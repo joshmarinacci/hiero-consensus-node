71f6b183169b293014027926a7ee1871847f2856
Josh Marinacci <joshua@marinacci.org>
2025-09-18 10:02:39 -0700

    Add Javadocs to FeeModel Add the rest of the HederFunctionality fee models to the registry
    Signed-off-by: Josh Marinacci <joshua@marinacci.org>

--- hedera-node/hapi-fees/src/main/java/org/hiero/hapi/fees/FeeModel.java
+++ hedera-node/hapi-fees/src/main/java/org/hiero/hapi/fees/FeeModel.java
@@ -6,10 +6,19 @@ import com.hedera.hapi.node.transaction.ExchangeRate;
 import java.util.Map;
 import org.hiero.hapi.support.fees.FeeSchedule;
 
+/**
+ * FeeModel represents logic to calculate the fees
+ * for a transaction. Many transactions can be handled
+ * by the common BaseFeeModel but some might need completely
+ * custom implementation.
+ */
 public interface FeeModel {
+    /** Get the @{@link HederaFunctionality} that this fee module handles. */
     HederaFunctionality getApi();
 
+    /** A string description of this fee model. Used for human-readable output. */
     String getDescription();
 
+    /** Compute the fee for a specific transaction. */
     FeeResult computeFee(Map<String, Object> params, ExchangeRate exchangeRate, FeeSchedule feeSchedule);
 }
