c02296bae1d836578aea8e45e48eb769a41c557f
Kelly Greco <82919061+poulok@users.noreply.github.com>
2025-10-02 14:44:14 -0500

    chore: Convert `ConsistentyTestingTool` JRS tests to Otter (#21358)
    Signed-off-by: Kelly Greco <kelly@swirldslabs.com>

--- platform-sdk/consensus-otter-tests/src/testIntegration/java/org/hiero/otter/test/ReconnectTest.java
+++ platform-sdk/consensus-otter-tests/src/testIntegration/java/org/hiero/otter/test/ReconnectTest.java
@@ -13,6 +13,7 @@ import static org.hiero.otter.fixtures.assertions.StatusProgressionStep.target;
 
 import com.swirlds.common.test.fixtures.WeightGenerators;
 import com.swirlds.platform.consensus.ConsensusConfig_;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import java.time.Duration;
 import org.hiero.otter.fixtures.Capability;
 import org.hiero.otter.fixtures.Network;
@@ -28,11 +29,16 @@ import org.hiero.otter.fixtures.result.SingleNodePlatformStatusResult;
  */
 public class ReconnectTest {
 
-    private static final long ROUNDS_NON_ANCIENT = 50L;
     private static final long ROUNDS_EXPIRED = 100L;
 
+    /**
+     * Tests that a node which is killed, kept down until it is behind, and then restarted is able to reconnect to the
+     * network and catch up with consensus.
+     *
+     * @param env the test environment
+     */
     @OtterTest(requires = Capability.RECONNECT)
-    void testSimpleNodeDeathReconnect(final TestEnvironment env) {
+    void testNodeDeathReconnect(@NonNull final TestEnvironment env) {
         final Network network = env.network();
         final TimeManager timeManager = env.timeManager();
 
@@ -43,8 +49,7 @@ public class ReconnectTest {
         network.addNodes(4);
 
         // Set the rounds non-ancient and expired to smaller values to allow nodes to fall behind quickly
-        network.withConfigValue(ConsensusConfig_.ROUNDS_NON_ANCIENT, ROUNDS_NON_ANCIENT)
-                .withConfigValue(ConsensusConfig_.ROUNDS_EXPIRED, ROUNDS_EXPIRED);
+        network.withConfigValue(ConsensusConfig_.ROUNDS_EXPIRED, ROUNDS_EXPIRED);
 
         // Set the node we will force to reconnect
         final Node nodeToReconnect = network.nodes().getLast();
@@ -59,7 +64,7 @@ public class ReconnectTest {
                 .hasMaximumTreeInitializationTime(Duration.ofSeconds(1));
         network.start();
 
-        // Wait for thirty seconds
+        // Allow the nodes to run for a short time
         timeManager.waitFor(Duration.ofSeconds(5L));
 
         // Shutdown the node for a period of time so that it falls behind.
@@ -91,7 +96,7 @@ public class ReconnectTest {
         // Validations
         assertThat(network.newLogResults()).haveNoErrorLevelMessages();
 
-        assertThat(nodeToReconnect.newReconnectResult()).hasNoFailedReconnects().hasExactSuccessfulReconnects(1);
+        assertThat(nodeToReconnect.newReconnectResult()).hasExactSuccessfulReconnects(1);
 
         assertThat(network.newConsensusResults().suppressingNode(nodeToReconnect))
                 .haveConsistentRounds();
