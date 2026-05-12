e11d181eb869175b23753693bc79c77216d7ecd7
Michael Tinker <michael.tinker@swirldslabs.com>
2026-05-12 09:20:35 -0500

    chore: Disable hooks by default (#25406)
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>
    Co-authored-by: Akram AbouEmara <508522+akdev@users.noreply.github.com>

--- hedera-node/test-clients/build.gradle.kts
+++ hedera-node/test-clients/build.gradle.kts
@@ -220,7 +220,7 @@ val prCheckPropOverrides =
         "hapiTestMiscRecordsSerial" to
             "blockStream.streamMode=RECORDS,nodes.nodeRewardsEnabled=false,quiescence.enabled=true,block.stateproof.verification.enabled=true",
         "hapiTestSimpleFees" to
-            "fees.simpleFeesEnabled=true,hedera.transaction.maximumPermissibleUnhealthySeconds=5",
+            "fees.simpleFeesEnabled=true,hedera.transaction.maximumPermissibleUnhealthySeconds=5,hooks.hooksEnabled=true",
         "hapiTestSimpleFeesSerial" to "fees.simpleFeesEnabled=true",
         "hapiTestNDReconnect" to "block.stateproof.verification.enabled=true",
         "hapiTestAtomicBatch" to
